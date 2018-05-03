2150111b30bf5bf52a874f7852ee0e4b3e4484d4
Li Xiao <xiao.li@siyanhui.com>
2018-05-03 16:48:25 +0800

    引入 BQMM_Lib v2.1.0

---
+++ uikit/src/com/netease/nim/uikit/common/BQMMConstants.java
@@ -0,0 +1,13 @@
+package com.netease.nim.uikit.common;
+
+/**
+ * Created by lixiao on 18-3-29.
+ * BQMM集成 增加用于标识消息类型的常量
+ */
+
+public class BQMMConstants {
+    public static final String BQMM_MSG_TYPE = "txt_msgType";
+    public static final String BQMM_MSG_TYPE_EMOJI = "emojitype";//文字和表情混排类型
+    public static final String BQMM_MSG_TYPE_STICKER = "facetype";//大表情类型
+    public static final String BQMM_MSG_DATA="msg_data";
+}
---
+++ uikit/src/com/netease/nim/uikit/common/bqmmgif/IBqmmSendGifListener.java
@@ -0,0 +1,11 @@
+package com.netease.nim.uikit.common.bqmmgif;
+
+import com.melink.bqmmsdk.bean.BQMMGif;
+
+/**
+ * Created by darrenfantasy on 2017/12/11.
+ */
+
+public interface IBqmmSendGifListener {
+    void onSendBQMMGif(BQMMGif bqmmGif);
+}
